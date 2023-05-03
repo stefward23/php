da97671a2f11b5e43fdfef1f1a233fae866d8251
Stefan Ward <stefmward23@gmail.com>
2023-05-02 20:25:12 -0400

    arrays

---
+++ arrays.php
@@ -0,0 +1,20 @@
+<?php
+
+$foods = array("apple","orange","banana", "coconut");
+
+// echo $foods[0] . "<br>";
+// $foods[0] = "pineapple";
+// array_unshift($foods, "pineapple"); push to beginning of array
+// array_push($foods, "kiwi");
+// array_pop($foods);
+// array_shift($foods); 
+// $foods = array_reverse($foods);
+echo count($foods);
+
+
+foreach($foods as $food) {
+    echo $food . "<br>";
+    
+}
+
+?>
---
+++ forandwhileloops.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="index.php" method="post">
+        <input type="submit" name="stop" value="stop">
+    </form>
+</body>
+</html>
+<?php
+
+    // for($i = 0; $i < 2; $i++){
+    //     echo"Hello <br>";
+    // }
+
+    // $seconds = 0;
+    // $running = true;
+
+    // while($running){
+    //   if(isset($_POST["stop"])){
+    //     $running = false;
+    //   }
+    //   else{
+    //     $seconds++;
+    //     echo $seconds . "<br>";
+    //   }
+       
+    // }
+
+?>
