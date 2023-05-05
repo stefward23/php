5015c8c71569aa1d88e1623a72fb3facbf367052
Stefan Ward <stefmward23@gmail.com>
2023-05-05 13:28:27 -0400

    session

---
+++ home.php
@@ -0,0 +1,35 @@
+<?php
+
+    session_start();
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Session</title>
+</head>
+<body>
+    <p>This is the Home page</p>
+
+    <form action="home.php" method="post">
+        <input type="submit" name="logout" value="logout">
+    </form>
+
+
+</body>
+</html>
+
+<?php
+
+    if(isset($_POST["logout"])){
+        session_destroy();
+        header("Location: session.php");
+    }
+
+    // echo $_SESSION["username"] . "<br>";
+    // echo $_SESSION["password"] . "<br>";
+?>
